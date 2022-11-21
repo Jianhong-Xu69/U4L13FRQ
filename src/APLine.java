ffe17cd302f8ddede2c51accc68f02cc43971543
BT_4N2_14 <jxu5406@bths.edu>
2022-11-21 12:17:49 -0500

    Patchnotes v1.00
    Stuff

---
+++ src/APLine.java
@@ -0,0 +1,17 @@
+public class APLine {
+    private int a;
+    private int b;
+    private int c;
+    public APLine(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double getSlope(){
+        return (double) -a / (double) b;
+    }
+    public boolean isOnLine(int x, int y){
+        return ((a * x) + (b * y) + c == 0);
+    }
+
+}
